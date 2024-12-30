4a3c85d296d10058d4d9c7ba77407160ee3058d6
Beloved1310 <adeoluwafisayomi@gmail.com>
2024-12-30 13:23:18 +0100

    feat: generate services modules and controller

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,9 +1,12 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { UsersModule } from './users/users.module';
+import { OrdersModule } from './orders/orders.module';
+import { ChatModule } from './chat/chat.module';
 
 @Module({
-  imports: [],
+  imports: [UsersModule, OrdersModule, ChatModule],
   controllers: [AppController],
   providers: [AppService],
 })
