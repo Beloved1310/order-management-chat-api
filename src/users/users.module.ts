0790047c2786d42c21d4aa2b084ebe68e4d9bf38
Beloved1310 <adeoluwafisayomi@gmail.com>
2025-01-02 15:41:33 +0100

    fix: remove dead code

--- src/users/users.module.ts
+++ src/users/users.module.ts
@@ -2,9 +2,6 @@ import { Module } from '@nestjs/common';
 import { JwtModule } from '@nestjs/jwt';
 import { UsersService } from './users.service';
 import { UsersController } from './users.controller';
-import { AuthGuard } from '../guards/auth.guard';
-import { APP_GUARD } from '@nestjs/core';
-import { RolesGuard } from 'src/guards/roles.guard';
 @Module({
   imports: [
     JwtModule.register({
@@ -15,14 +12,6 @@ import { RolesGuard } from 'src/guards/roles.guard';
   ],
   providers: [
     UsersService,
-    // {
-    //   provide: APP_GUARD,
-    //   useClass: AuthGuard, // Global AuthGuard to run first
-    // },
-    // {
-    //   provide: APP_GUARD,
-    //   useClass: RolesGuard, // Global RolesGuard to run after AuthGuard
-    // },
   ],
   controllers: [UsersController],
   exports: [UsersService],
